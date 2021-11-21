7475d2d2248f227560ec78ff629deca3385a479e
jo15122002 <lapinette2002@ymail.com>
2021-11-21 15:51:06 +0100

    test deploy for message

--- main.js
+++ main.js
@@ -153,13 +153,41 @@ document.querySelectorAll('href').forEach(anchor => {
   });
 });
 
+//Test if element is in viewPort or not
 let observer = new IntersectionObserver(handler);
 categoryContainers.forEach(element => {
   observer.observe(element);
 });
 
 
-
+//Contact Form in PHP
+const form = document.querySelector("#Contact form"),
+statusTxt = form.querySelector(".button-area span");
+form.onsubmit = (e)=>{
+  e.preventDefault();
+  statusTxt.style.color = "#0D6EFD";
+  statusTxt.style.display = "block";
+  form.classList.add("disabled");
+  let xhr = new XMLHttpRequest();
+  xhr.open("POST", "message.php", true);
+  xhr.onload = ()=>{
+    if(xhr.readyState == 4 && xhr.status == 200){
+      let response = xhr.response;
+      if(response.indexOf("required") != -1 || response.indexOf("valid") != -1 || response.indexOf("failed") != -1){
+        statusTxt.style.color = "red";
+      }else{
+        form.reset();
+        setTimeout(()=>{
+          statusTxt.style.display = "none";
+        }, 3000);
+      }
+     // statusTxt.innerText = response;
+      form.classList.remove("disabled");
+    }
+  }
+  let formData = new FormData(form);
+  xhr.send(formData);
+}
 
 
 
